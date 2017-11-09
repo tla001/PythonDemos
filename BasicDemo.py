c676a43ec535e89d1c17b310cb8103a62f8b7e69
tla001 <1414419047@qq.com>
2017-11-09 20:52:49 +0800

    build project

---
+++ BasicDemo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-11-09 20:47:56
+# @Author  : tla001 (tla001@foxmail.com)
+# @Link    : www.tla001.cn
+# @Version : $Id$
+
+import os
